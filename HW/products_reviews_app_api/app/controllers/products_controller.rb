59d1275ac5486516e3cc3e5280927dfcd019726f
Clayton Miller <clayton.miller@loop.colum.edu>
2021-02-25 16:23:59 -0600

    Commit 7: Reviews index method fixed

--- HW/products_reviews_app_api/app/controllers/products_controller.rb
+++ HW/products_reviews_app_api/app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
+  before_action :set_product, only: [:show]
 
   # GET /products
   def index
-    @products = Product.all
+    reviews = Review.where(product_id: params[:product_id])
 
-    render json: @products, status: 200
+    render json: reviews
   end
 
   # GET /products/1
@@ -14,39 +14,10 @@ class ProductsController < ApplicationController
     render json: @product, status: 200
   end
 
-  # POST /products
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /products/1
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /products/1
-  def destroy
-    @product.destroy
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :price, :img)
     end
 end
--- HW/products_reviews_app_api/app/controllers/reviews_controller.rb
+++ HW/products_reviews_app_api/app/controllers/reviews_controller.rb
@@ -1,16 +1,11 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :update, :destroy]
+  before_action :set_review, only: [ :update, :destroy]
 
   # GET /reviews
   def index
-    @reviews = Review.all
+    product = Product.find(params[:product_id])
 
-    render json: @reviews
-  end
-
-  # GET /reviews/1
-  def show
-    render json: @review
+    render json: product.reviews
   end
 
   # POST /reviews
