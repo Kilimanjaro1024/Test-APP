a04b18eef19f1118f399c593f707762add4acd62
Clayton Miller <clayton.miller@loop.colum.edu>
2021-02-25 18:39:11 -0600

    Commit 11: AJAX request to Reviews Index

--- HW/som-app/src/App.js
+++ HW/som-app/src/App.js
@@ -1,31 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from "react"
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const url = "http://localhost:3000/"
+  const url = "http://localhost:3000/";
 
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+
+  const [reviews, setReviews] = useState([]);
 
   const getProducts = () => {
     fetch(url + "/products")
       .then((response) => response.json())
-      .then((data) =>{
-        setProducts(data)
-      })
-  }
+      .then((data) => {
+        setProducts(data);
+      });
+  };
 
-  useEffect(() => getProducts(), [])
+  const getReviews = (id) => {
+    console.log("get");
+    fetch(url + "/products/" + id + "/reviews")
+      .then((response) => response.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  };
+
+  const 
+
+  const handleClick = (id) => {
+    getReviews(id);
+    console.log("hello");
+  };
+
+  useEffect(() => getProducts(), []);
+
   return (
     <div className="App">
       Hello
-      {products.map((product, index) =>(
-        <article key={index}>
-          <h1>{product.name}</h1>
-          <h1>{product.price}</h1>
-          <img src={product.img} alt="none"/>
-        </article>
+      {products.map((product, index) => (
+        <>
+          <article key={index}>
+            <h1>{product.name}</h1>
+            <h1>{product.price}</h1>
+            <button onClick={() => handleClick(product.id)}>
+              <img src={product.img} alt="none" />
+            </button>
+          </article>
+        </>
       ))}
+      {reviews.map((review, index) => {
+        return (
+          <article key={index}>
+            <h1>{review.title}</h1>
+            <h1>{review.content}</h1>
+            <h1>{review.author}</h1>
+          </article>
+        );
+      })}
     </div>
   );
 }
